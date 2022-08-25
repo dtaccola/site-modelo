763c962ccd4dae9f96d48d9be5dbdbfcea485e4e
dtaccola <danieltaccola@yahoo.com.br>
2022-08-25 17:03:17 -0300

    estilização do accordion

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function VerifyActive(item, content, content_actived) {
   const icon_item = item.querySelector(".icon");
   const icon_item_active = document.querySelectorAll(".icon");
 
-  icon_item_active.forEach((item) => (item.innerHTML = "+"));
+  icon_item_active.forEach((item) => (item.innerHTML = "&#9660;"));
 
   if (content_actived) {
     content_actived.style.height = 0;
@@ -24,7 +24,7 @@ function VerifyActive(item, content, content_actived) {
   }
 
   if (content !== content_actived) {
-    icon_item.innerHTML = "-";
+    icon_item.innerHTML = "&#9650;";
     content.classList.add("active");
     content.style.height = content.scrollHeight + 10 + "px";
   }
